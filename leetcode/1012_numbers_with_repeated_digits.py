0c0c37310bb67f2712f172c21b73ae1cdab92d7e
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-12-30 20:04:04 -0300

    :recycle: Green: Succesfully created function that works, but it's too slow

--- leetcode/1012_numbers_with_repeated_digits.py
+++ leetcode/1012_numbers_with_repeated_digits.py
@@ -3,7 +3,7 @@ Given a positive integer N, return the number of positive integers less than or 
 
  
 
-Example 1:
+Example 1:numDupDigitsAtMostN
 
 Input: 20
 Output: 1
@@ -25,7 +25,7 @@ Note:
 '''
 import pytest
 
-@pytest.mark.parametrize('input_and_output', [(20, 1), (100, 10), (1000, 262)])
+@pytest.mark.parametrize('input_and_output', [(20, 1), (100, 10), (1000, 262), (2000, 758), (738935, 609230)])
 def test_num_dup_digits_at_most_n(input_and_output):
   input_digit = input_and_output[0]
   expected_output = input_and_output[1]
@@ -35,4 +35,36 @@ def test_num_dup_digits_at_most_n(input_and_output):
 
 
 def numDupDigitsAtMostN(N: int) -> int:
-  return N
+    list_counting = pre_calculation(N)
+    return list_counting[N]
+
+def has_duplicate(natural_number) -> bool:
+    ''' :Params natural number
+        :Returns True if input has any repeated digit
+                or False if not
+    '''
+    if natural_number < 10:
+        return False
+    digits_in_number = {}
+    while(natural_number > 0):
+        n = natural_number%10
+        if n in digits_in_number:
+            return True
+        else:
+            digits_in_number[n] = 1
+        natural_number = natural_number//10
+    return False
+
+def pre_calculation(value_to_count) -> list:
+    ''' :Params value_to_count as integer
+        :Returns list of how many duplicate numbers till the given index
+    '''
+    global duplicate_count
+    duplicate_count = []
+    duplicate_count.append(0)
+    for i in range(1, value_to_count+1):
+        if has_duplicate(i):
+            duplicate_count.append( 1 + duplicate_count[i-1])
+        else:
+            duplicate_count.append( 0 + duplicate_count[i-1])
+    
