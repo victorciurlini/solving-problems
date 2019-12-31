0ed33f782e6caa0906ac85295f22b42254c24800
Thiago Luis <thiagoluisrpinho@gmail.com>
2019-12-31 17:47:51 -0300

    :recycle: Green: Even quicker, but wont work for some reason at leetcode

--- leetcode/1012_numbers_with_repeated_digits.py
+++ leetcode/1012_numbers_with_repeated_digits.py
@@ -24,16 +24,24 @@ Note:
 1 <= N <= 10^9
 '''
 import pytest
-
-@pytest.mark.parametrize('input_and_output', [(20, 1), (100, 10), (1000, 262), (2000, 758), (738935, 609230)])
+duplicate_count = {}
+@pytest.mark.parametrize('input_and_output', [(20, 1), (21, 1), (100, 10), (1000, 262), (2000, 758), (738935, 609230)])
 def test_num_dup_digits_at_most_n(input_and_output):
 
-  input_digit = input_and_output[0]
-  expected_output = input_and_output[1]
-  predicted_output = numDupDigitsAtMostN(input_digit)
+    input_digit = input_and_output[0]
+    expected_output = input_and_output[1]
+    predicted_output = numDupDigitsAtMostN(input_digit)
 
-  assert predicted_output == expected_output
+    assert predicted_output == expected_output
 
+@pytest.mark.parametrize('input_and_output', [(1, 9), (2, 90), (3, 738)])
+def test_combination_calculate_non_duplicate(input_and_output):
+
+    input_digit = input_and_output[0]
+    expected_output = input_and_output[1]
+    predicted_output = combination_calculate_non_duplicate(input_digit)
+
+    assert predicted_output == expected_output
 
 def numDupDigitsAtMostN(N: int) -> int:
     number = smart_brute_force(N)
@@ -44,16 +52,13 @@ def has_duplicate(natural_number) -> bool:
         :Returns True if input has any repeated digit
                 or False if not
     '''
-    if natural_number < 10:
-        return False
-    digits_in_number = {}
-    while(natural_number > 0):
-        n = natural_number%10
-        if n in digits_in_number:
+    stringified_natural_number = str(natural_number)
+    digits_collected = {}
+    for digit in stringified_natural_number:
+        if digit in digits_collected:
             return True
         else:
-            digits_in_number[n] = 1
-        natural_number = natural_number//10
+            digits_collected[digit] = 1
     return False
 
 def smart_brute_force(value_to_count: int) -> dict:
@@ -63,51 +68,43 @@ def smart_brute_force(value_to_count: int) -> dict:
     stringfied_digits = str(value_to_count)
     number_of_digits = len(stringfied_digits)
     start_number_to_count = 10**(number_of_digits-1) - 1
-    start_amount_to_count = start_number_to_count - combination_calculate( number_of_digits- 1)
-    duplicate_count = {}
-    duplicate_count[start_number_to_count] = start_amount_to_count
+    start_amount_to_count = start_number_to_count - combination_calculate_non_duplicate( number_of_digits- 1)
+    global duplicate_count
+    if not start_number_to_count in duplicate_count:
+        duplicate_count[start_number_to_count] = start_amount_to_count
+    else: 
+        start_number_to_count = max(duplicate_count.keys())
+        print(start_amount_to_count)
     for i in range(start_number_to_count+1,value_to_count+1):
         if has_duplicate(i):
             duplicate_count[i] = 1 + duplicate_count[i-1]
         else:
             duplicate_count[i] = 0 + duplicate_count[i-1]
-    return  duplicate_count[value_to_count]
+    return    duplicate_count[value_to_count]
 
 
-def combination_calculate(number_of_digits: int) -> int: 
+def combination_calculate_non_duplicate(number_of_digits: int) -> int: 
     ''' :Params The number of digits
-        :Returns how many numbers with replicate digits this number of digits have
+        :Returns how many numbers without duplicate digits exists with this number of digits
     '''
     amount_of_duplicate_numbers = 9
-    if number_of_digits > 1:
-        amount_of_duplicate_numbers += 9*9
-        if number_of_digits > 2:
-            amount_of_duplicate_numbers += 9*9*8
-            if number_of_digits > 3:
-                amount_of_duplicate_numbers += 9*9*8*7
-                if number_of_digits > 4:
-                    amount_of_duplicate_numbers += 9*9*8*7*6
-                    if number_of_digits > 5:
-                        amount_of_duplicate_numbers += 9*9*8*7*6*5
-                        if number_of_digits > 6:
-                            amount_of_duplicate_numbers += 9*9*8*7*6*5*4
-                            if number_of_digits > 7:
-                                amount_of_duplicate_numbers += 9*9*8*7*6*5*4*3
-                                if number_of_digits > 8:
-                                    amount_of_duplicate_numbers += 9*9*8*7*6*5*4*3*2
-
+    combination_possible = 9
+    for i in range(1, number_of_digits):
+    # For instance for 2 digits we got 9+9*9
+        combination_possible *= (10-i)
+        amount_of_duplicate_numbers += combination_possible
     return amount_of_duplicate_numbers
 
 def pre_calculation(value_to_count) -> None:
-    ''' :Params value_to_count as integer
-        :Returns list of how many duplicate numbers till the given index
-    '''
-    global duplicate_count
-    duplicate_count = []
-    duplicate_count.append(0)
-    for i in range(1, value_to_count+1):
-        if has_duplicate(i):
-            duplicate_count.append( 1 + duplicate_count[i-1])
-        else:
-            duplicate_count.append( 0 + duplicate_count[i-1])
+      ''' :Params value_to_count as integer
+          :Returns list of how many duplicate numbers till the given index
+      '''
+      global duplicate_count
+      duplicate_count = []
+      duplicate_count.append(0)
+      for i in range(1, value_to_count+1):
+          if has_duplicate(i):
+              duplicate_count.append( 1 + duplicate_count[i-1])
+          else:
+              duplicate_count.append( 0 + duplicate_count[i-1])
 
