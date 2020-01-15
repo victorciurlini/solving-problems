275e23f0cec7abdd313e11f5fa52846091473d57
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-15 15:06:51 -0300

    :recycle: Green: 283 now faster than 90.8%  of the community

--- leetcode/283_move_zeroes.py
+++ leetcode/283_move_zeroes.py
@@ -28,13 +28,14 @@ def moveZeroes(nums: List[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
-    zero_indexes = []
-    for i in range(len(nums)):
+    i = 0
+    zero_count = 0
+    while i < len(nums):
         if nums[i] == 0:
-            nums.append(0)
-            zero_indexes.append(i)
-    index_decrement = 0
-    for index in zero_indexes:
-        nums.pop(index + index_decrement)
-        index_decrement -= 1
-            
+            zero_count += 1
+            nums.pop(i)
+        else:
+            i += 1
+    while zero_count:
+        nums.append(0)
+        zero_count -= 1
