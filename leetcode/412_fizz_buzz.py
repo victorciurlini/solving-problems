e99b5d699c191e10f0d1389ad729f70f114d3da9
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-15 13:51:31 -0300

    :recycle: Green: Solved 412 faster than 75.52% of the community

--- leetcode/412_fizz_buzz.py
+++ leetcode/412_fizz_buzz.py
@@ -64,4 +64,14 @@ def test_fizz_buzz(input_and_output):
     assert expected_output == predicted_output
 
 def fizzBuzz(n: int) -> List[str]:
-    return []
+    fizz_buzz_list = []
+    for i in range(1, n+1):
+        word = ""
+        if i%3 == 0:
+            word += "Fizz"
+        if i%5 == 0:
+            word += "Buzz"
+        if len(word) == 0:
+            word = str(i)
+        fizz_buzz_list.append(word)
+    return fizz_buzz_list
