d3289c095f8776c8cd8c7fa1c25317de6118387b
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-01-15 09:24:28 -0300

    :recycle: Green: Solved quicker than 64.27%

--- leetcode/136_single_number.py
+++ leetcode/136_single_number.py
@@ -6,18 +6,18 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 
 Example 1:
 
-Input: [2,2,1]
+Input: [2, 2, 1]
 Output: 1
 Example 2:
 
-Input: [4,1,2,1,2]
+Input: [4, 1, 2, 1, 2]
 Output: 4'''
 import pytest
 from typing import List
 
 @pytest.mark.parametrize('input_and_output', [
-    ([2,2,1], 1),
-    ([4,1,2,1,2], 4)])
+    ([2, 2, 1], 1),
+    ([4, 1, 2, 1, 2], 4)])
 def test_single_number(input_and_output):
     input_list = input_and_output[0]
     expected_output = input_and_output[1]
@@ -26,4 +26,14 @@ def test_single_number(input_and_output):
 
 
 def singleNumber(nums: List[int]) -> int:
-    return False
+    open_set = set()
+    closed_set = set()
+    for num in nums:
+        if num in closed_set:
+            continue
+        if num in open_set:
+            closed_set.add(num)
+            open_set.remove(num)
+        else:
+            open_set.add(num)
+    return open_set.pop()
